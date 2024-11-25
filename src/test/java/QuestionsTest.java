e26d833d9cef340efd48cfd18e7f0c1c6014aeb3
bulpin <dj-sereg@ya.ru>
2024-11-25 20:35:26 +0300

    Sprint_4

--- src/test/java/QuestionsTest.java
+++ src/test/java/QuestionsTest.java
@@ -2,6 +2,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,8 +12,20 @@ import pageobject.MainPage;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Parameterized.class)
 public class QuestionsTest {
+
     private WebDriver driver;
+    private final int questionIndex;
+
+    public QuestionsTest(int questionIndex) {
+        this.questionIndex = questionIndex;
+    }
+    // Данные для параметризации
+    @Parameterized.Parameters
+    public static Object[] getQuestionsIndices() {
+        return new Object[]{0, 1, 2, 3, 4, 5, 6, 7}; // Индексы вопросов
+    }
 
     @Before
     public void setUp() {
@@ -25,21 +39,23 @@ public class QuestionsTest {
     public void testQuestionsTextIsVisible() {
         MainPage mainPage = new MainPage(driver);
 
-        // Цикл проверки всех вопросов
-        for (int i = 0; i < 8; i++) {
-            mainPage.scrollToQuestion(i); // Скроллим к вопросу
-            mainPage.clickQuestion(i);   // Кликаем по вопросу
-            String answer = mainPage.getAnswerText(i); // Получаем текст ответа
+        // Скроллим к вопросу
+        mainPage.scrollToQuestion(questionIndex);
 
-            // Проверяем, что текст ответа отображается
-            assertFalse("Ответ на вопрос №" + (i + 1) + " не отображается!", answer.isEmpty());
-        }
+        // Кликаем по вопросу
+        mainPage.clickQuestion(questionIndex);
+
+        // Получаем текст ответа
+        String answer = mainPage.getAnswerText(questionIndex);
+
+        // Проверяем, что текст ответа отображается
+        assertFalse("Ответ на вопрос №" + (questionIndex + 1) + " не отображается!", answer.isEmpty());
     }
+
     @After
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
     }
-
 }
